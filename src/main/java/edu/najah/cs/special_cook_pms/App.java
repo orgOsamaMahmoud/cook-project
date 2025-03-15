18cfe312640f20b6d1b1968e14e51eeaf977cf1e
Abo-AL-Yaseen <mahmoodyassen3@gmail.com>
2025-03-16 01:21:33 +0200

    first commit

---
+++ src/main/java/edu/najah/cs/special_cook_pms/App.java
@@ -0,0 +1,13 @@
+package edu.najah.cs.special_cook_pms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
